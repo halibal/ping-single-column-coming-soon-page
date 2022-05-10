0491d169d83965c7fa70355d52eb4e5c612a9aa5
Halibal <halibal95@gmail.com>
2022-05-10 15:35:27 +0300

    html and css completed

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const inputEmail = document.getElementById('inputEmail');
 const warningText = document.querySelector('.warningText');
+
+
+// inputEmail.addEventListener('input', inputFunction);
+
+// function inputFunction() {
+//     warningText.innerHTML = `Please provide a valid email address`
+
+// }
